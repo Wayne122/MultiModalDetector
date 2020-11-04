31567549546dd3f07910f5ee81ddc04ef277a840
Wayne122 <ww040702tw01@gmail.com>
2020-11-04 13:32:44 -0800

    test

--- MyDetector/Yolov4Detector.py
+++ MyDetector/Yolov4Detector.py
@@ -118,11 +118,12 @@ class Yolov4Detector(object):
                 for *xyxy, conf, cls in det:
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        print('IIIIIIITTTTTTTTSSSSSSSSS')
-                        print(xywh)
-                        print('HEEEEEERRRRRRRRRREEEEEEE')
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
+
+                    print('IIIIIIITTTTTTTTSSSSSSSSS')
+                    print(xyxy)
+                    print('HEEEEEERRRRRRRRRREEEEEEE')
 
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
